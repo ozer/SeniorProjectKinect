ccbbb7d8b412b31d25cda30367e841f5e88bf629
Mert Asim Karaoglu <mertakaraoglu@gmail.com>
2018-02-23 22:33:02 +0300

    Calibration
    Worked on

--- Code/calibration.m
+++ Code/calibration.m
@@ -10,8 +10,10 @@ start([rgbImage depthImage]);
 [imgColor, ts_color, metaData_Color] = getdata(rgbImage);
 [imgDepth, ts_depth, metaData_Depth] = getdata(depthImage);
 
-imwrite(uint8(imgColor(:, :, 1:3)), 'rgb_1_10.png');
+image_sym = fliplr(imgColor(:, :, 1:3));
+
+imwrite(uint8(image_sym), 'rgb_1_22.png');
 
 %% Calibration
 
-stereoCameraCalibrator('folder1','folder2',33,'millimeters');
+stereoCameraCalibrator('folder1','folder2',58,'millimeters');
