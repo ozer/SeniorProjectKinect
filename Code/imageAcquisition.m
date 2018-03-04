7c00edb0b4699a8d1d6fbb6bbae8efe8fa930f45
Mert Asim Karaoglu <mertakaraoglu@gmail.com>
2018-03-04 22:20:22 +0300

    Getting the body joints

--- Code/imageAcquisition.m
+++ Code/imageAcquisition.m
@@ -3,7 +3,7 @@
 mex -setup C++
 run('D:\OneDrive\Belgeler\MATLAB\Add-Ons\Collections\Kinect 2 Interface for Matlab\code\jrterven-Kin2-afeec4f\compile_cpp_files.m');
 
-%%
+%% Depth and Colored image acquisition
 
 clear all; close all; clc;
 
@@ -35,3 +35,24 @@ valid_frame_percentage = valid/100
 
 k2.delete;
 toc
+
+%% 
+
+clear all;
+
+k2 = Kin2('color','depth','body')
+
+pause(4)
+
+validData = k2.updateData;
+
+color = k2.getColor;
+ptDepth = k2.mapCameraPoints2Depth(color)
+depth = k2.getDepth;
+ptColor = k2.mapDepthPoints2Color(depth);
+bodies = k2.getBodies;
+deneme = bodies(1).Position
+posDepth = k2.mapCameraPoints2Depth(bodies(1).Position)
+posColor = k2.mapCameraPoints2Color(bodies(1).Position)
+
+k2.delete;
